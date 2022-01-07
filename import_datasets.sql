e735fbc3d1470bcdbe3ba6ef54764a9ebd4571a0
Shem Jehro Jondanero <shemjehrojondanero@gmail.com>
2022-01-07 19:00:51 +0800

    added sql files for number 1 and 2

---
+++ import_datasets.sql
@@ -0,0 +1,7 @@
+COPY vaccines_by_location ( vbl_location, vbl_iso_code, vbl_vaccines, vbl_last__observation_date, vbl_source_name, vbl_source_website )
+FROM '/2121Finals_datasets/locations.csv' 
+WITH CSV HEADER;
+
+COPY vaccinations_by_manufacturer (vbm_location, vbm_date, vbm_vaccine, vbm_total_vaccinations ) 
+FROM '/2121Finals_datasets/vaccinations-by-manufacturer.csv' 
+WITH CSV HEADER;

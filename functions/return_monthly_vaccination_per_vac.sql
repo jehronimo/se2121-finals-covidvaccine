fcfc775dea431706f6c532fbac3cfbd3b2bae5a7
Shem Jehro Jondanero <shemjehrojondanero@gmail.com>
2022-01-10 20:59:09 +0800

    added get_vaccinations_per_vaccine function sql file

--- functions/return_monthly_vaccination_per_vac.sql
+++ functions/return_monthly_vaccination_per_vac.sql
@@ -1,31 +1,30 @@
-CREATE FUNCTION  return_monthly_vaccinations_per_vac(loc_name VARCHAR(90), yyyy_mm_str VARCHAR(10)) 
-    RETURNS VOID
+CREATE FUNCTION get_vaccinations_per_vaccine (
+  loc VARCHAR(90),
+  yyyy_mm_str VARCHAR(10)
+)
+RETURNS VOID
 LANGUAGE plpgsql
 AS
 $$
 DECLARE
-var_r record;
-output_str_arr TEXT[];
-var_str VARCHAR(90);
-var_date VARCHAR(10);
-output_str TEXT := '';
+  var_r record;
+BEGIN
+  RAISE NOTICE 'For the month of %:', TO_CHAR(TO_TIMESTAMP(yyyy_mm_str, 'YYYY-MM'), 'Mon. YYYY');
+  FOR var_r IN 
+    SELECT vbm_vaccine, SUM(vbm_total_vaccinations) AS vaccination_per_vac
+    FROM (SELECT vbm_vaccine, vbm_total_vaccinations, vbm_location, vbm_date 
+          FROM vaccinations_by_manufacturer
+          WHERE vbm_location = loc AND TO_CHAR(TO_TIMESTAMP(yyyy_mm_str, 'YYYY-MM'), 'Mon. YYYY') = TO_CHAR(vbm_date, 'Mon. YYYY')
+          GROUP BY vbm_vaccine, vbm_total_vaccinations, vbm_location, vbm_date ) as query
+    GROUP BY vbm_vaccine 
 
-BEGIN	
-    FOR var_r IN (SELECT vbm_vaccine AS vaccine, vbm_total_vaccinations AS total_vaccinations, vbm_date AS date_var
-                FROM vaccinations_by_manufacturer
-                WHERE vbm_location = loc_name AND TO_CHAR(vbm_date, 'YYYY-MM') = yyyy_mm_str)
-    
-        LOOP
-            -- var_date := TO_CHAR(var_r.date_var, 'Mon. YYYY');
-            output_str = output_str + ('% has % vaccinations, ', var_r.vaccine, var_r.total_vaccinations);
-        END LOOP
+      LOOP
+      RAISE NOTICE ' % has % vaccinations,', var_r.vbm_vaccine, var_r.vaccination_per_vac;
+      END LOOP;
 
-    -- FOREACH var_str IN ARRAY output_str_arr
+    IF NOT FOUND THEN 
+      RAISE NOTICE 'the country is non-existing or the date is invalid.';
+    END IF;
 
-    --     LOOP
-    --         concat(output_str, var_str);
-    --     END LOOP;
-
-    RAISE_NOTICE output_str;
-            
-END; $$ 
+END;
+$$
